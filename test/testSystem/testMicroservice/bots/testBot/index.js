a4b0e574ca452bd4f21491f144232891779a9879
Joseph Lee Hunsaker <joseph.l.hunsaker@gmail.com>
2018-06-23 14:30:25 -0600

    test system

---
+++ test/testSystem/testMicroservice/bots/testBot/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const leo = require("leo-sdk");
+const ls = leo.streams;
+
+
+exports.handler = function(event, context, callback) {
+	const ID = context.botId;
+
+	// Do work
+	callback();
+};
